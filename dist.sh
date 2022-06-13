7ed509f9c926f63653cc21ac142be2e027c8e1e3
IbsanjU <bharath2668@gmail.com>
2022-06-13 08:54:16 +0530

    Update dist.sh

--- dist.sh
+++ dist.sh
@@ -2,14 +2,16 @@
 if [ -z "$1" ]
 then
   echo "Which folder do you want to deploy to GitHub Pages?"
+  echo "example: dist.sh ./build/web/"
   exit 1
 fi
 
+echo $1
+
 flutter pub get
 flutter build web
-cp $1 ./dist/web/
 
-git add .
+git add $1 -f
 git commit -m "web-build-$(date)"
 
-git subtree push --prefix ./dist/web/ origin gh-pages
+git subtree push --prefix $1 origin gh-pages
